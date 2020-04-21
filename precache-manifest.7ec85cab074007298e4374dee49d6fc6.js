64a7d5fc5574f2f6c41d5c7327197e50bbe6aff1
vanvong1989 <vanvong1989>
2020-04-21 22:20:55 +0700

    first commit

---
+++ precache-manifest.7ec85cab074007298e4374dee49d6fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b76d054604025a91f8152664b7070a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "868d6a24c50952eb16ac",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4637edb35a5d3c755751",
+    "url": "/static/js/2.eeadeae9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eeadeae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "868d6a24c50952eb16ac",
+    "url": "/static/js/main.cd8365d7.chunk.js"
+  },
+  {
+    "revision": "b1fd15836c42726712d0",
+    "url": "/static/js/runtime-main.2f7efdc7.js"
+  }
+]);
